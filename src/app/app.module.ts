c5d25bea7dadd4f9da5f5663bb023dec6e70494b
Andrew Davis <reign-andrew@Andrews-MacBook-Pro.local>
2018-12-05 14:44:41 -0300

    Lesson 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { SimpleFormComponent } from './simple-form/simple-form.component';
+import { AppComponent } from "./app.component";
+import { SimpleFormComponent } from "./simple-form/simple-form.component";
+import { MailService } from "./mail.service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SimpleFormComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, SimpleFormComponent],
+  imports: [BrowserModule],
+  providers: [MailService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/mail.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root"
+})
+export class MailService {
+  message = "You've got mail";
+
+  constructor() {}
+}
